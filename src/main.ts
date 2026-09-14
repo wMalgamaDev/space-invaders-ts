af786606c8e8d3fdc3d5f9b940be3b7a43877b18
wMalgama <kucerovv595@gmail.com>
2026-09-14 19:25:52 +0800

    feat: function gameLoop()

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,17 @@ const gameWindow = document.getElementById('gameWindow') as HTMLCanvasElement;
 const ctx = gameWindow.getContext('2d');
 
 const img: HTMLImageElement = new Image();
-
 img.src = "./public/assets/atlas.png";
+
+let timeDelta = 0;
+let prevTime = performance.now()/1000;
+
+function gameLoop(time: number): void{
+    const currTime = time/1000;
+    timeDelta = currTime-prevTime;
+    prevTime = currTime;
+    //console.log(timeDelta);
+    requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
