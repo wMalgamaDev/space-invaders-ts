c8b49a473a456d5500d98108191fed0d0c8719e3
wMalgama <kucerovv595@gmail.com>
2026-09-14 22:01:47 +0800

    feat: input system

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,27 @@
 const gameWindow = document.getElementById('gameWindow') as HTMLCanvasElement;
 const ctx = gameWindow.getContext('2d');
 
+//Input
+let keys: {[key: string]: boolean} = {};
+let dir: boolean;
+
+window.addEventListener('keydown', (e) => {
+    keys[e.code] = true;
+    switch(e.code){
+        case "KeyA":
+            dir = false;
+            break;
+        case "KeyD":
+            dir = true;
+            break;
+        default:
+            break;
+    }
+});
+window.addEventListener('keyup', (e) => {
+    keys[e.code] = false;
+});
+
 //Texture Loading
 const img: HTMLImageElement = new Image();
 img.src = "./public/assets/atlas.png";
